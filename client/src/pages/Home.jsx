65fda6a374191f78190004498a7216c19057ee0d
NeoBitose <ramadhanalif430@gmail.com>
2024-11-11 17:49:51 +0700

    feat: client, add filtering

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,16 +11,42 @@ function Home() {
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    const [shopName, setShopName] = useState("");
+    const [adminEmail, setAdminEmail] = useState("");
+    const [productName, setProductName] = useState("");
+    const [stock, setStock] = useState("");
+
     const [body, setBody] = useState({
         params: {
+            shopName: shopName,
+            adminEmail: adminEmail,
+            productName: productName,
+            stock: stock,
             page: activePage
         }
     })
+
+    const handlerFilter = () => {
+        setActivePage(1)
+        setBody({
+            params: {
+                shopName: shopName,
+                adminEmail: adminEmail,
+                productName: productName,
+                stock: stock,
+                page: activePage
+            }
+        })
+    }
 
     const handlerPagination = (pageSelect) => {
         setActivePage(pageSelect)
         setBody({
             params: {
+                shopName: shopName,
+                adminEmail: adminEmail,
+                productName: productName,
+                stock: stock,
                 page: pageSelect
             }
         })
@@ -75,15 +101,36 @@ function Home() {
             {loading && <p>...Loading</p>}
             {!error && (
                 <>
+                    <div id="shopForm" className="flex p-10 shadow-md text-left justify-center gap-5">
+                        <div className="">
+                            <label htmlFor="shopName" className="block text-gray-700 font-medium mb-2">Nama Toko</label>
+                            <input type="text" id="shopName" name="shopName" value={shopName} onChange={(e) => setShopName(e.target.value)} className="w-full text-black bg-white px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500" />
+                        </div>
+
+                        <div className="">
+                            <label htmlFor="productName" className="block text-gray-700 font-medium mb-2">Nama Produk</label>
+                            <input type="text" id="productName" name="productName" value={productName} onChange={(e) => setProductName(e.target.value)} className="w-full text-black bg-white px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500" />
+                        </div>
+
+                        <div className="">
+                            <label htmlFor="stock" className="block text-gray-700 font-medium mb-2">Stok</label>
+                            <input type="number" id="stock" name="stock" value={stock} onChange={(e) => setStock(e.target.value)} className="w-full text-black bg-white px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500" />
+                        </div>
+                        <div className="mt-7">
+                            <button onClick={() => handlerFilter()} className=" bg-green-500 text-white font-semibold rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500">
+                                Submit
+                            </button>
+                        </div>
+                    </div>
                     <section className="max-w-6xl mx-auto mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         {shops.map((shop, index) => (
                             <div key={index} className="p-4 border rounded-md bg-white shadow-md">
                                 <img src={shop.products[0].images[0]} alt="Car" className="w-full h-40 object-cover mb-4" />
                                 <h3 className="font-semibold text-black">{shop.name}</h3>
                                 <p className="text-green-500 font-bold">Rp {shop.products[0].price}</p>
-                                <p className="text-gray-600 mt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                <div className="flex items-center justify-between text-gray-500 text-sm mt-4"> <span>4 orang</span> <span>Manual</span> <span>Tahun 2020</span>
-                                </div>
+                                <p className="text-gray-600 mt-2 text-sm">Stock : {shop.products[0].stock}</p>
+                                {/* <div className="flex items-center justify-between text-gray-500 text-sm mt-4"> <span>4 orang</span> <span>Manual</span> <span>Tahun 2020</span>
+                                </div> */}
                                 <button className="w-full px-4 py-2 mt-4 text-white bg-green-500 rounded-md">Pilih Mobil</button>
                             </div>
                         ))}
